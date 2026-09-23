14b39e04ddc748dafcf3ec150daef7fb07591356
MIGUEL ROSA TAQUATIÁ <miguel_taquatia@estudante.sesisenai.org.br>
2026-09-23 17:06:32 -0300

    sei la alfa

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,30 @@
+import "./Header.css";
 
 function Header() {
-    return(
-        <header className="header">
-            <div className="logo">
-                <img className="logo-icon" src="#" alt="Logo" />
-                <span className="logo-text">Studio Alfa</span>
-            </div>
-            <nav className="nav">
-                <a href="#">Início</a>
-                <a href="#">Serviços</a>
-                <a href="#">Sobre</a>
-                <a href="#" className="btn-contatos">Contato</a>
-            </nav>
-        </header>
-    )
+  return (
+    <header className="header">
+      <div className="header-container">
+        <div className="logo">
+          <img
+            className="logo-icon"
+            src="\src\assets\imgs\Alfa-Romeo-Logo.png"
+            alt="logo"
+          ></img>
+          <span className="logo-text">Studio Alfa</span>
+        </div>
+      
+
+      <nav className="nav">
+        <a href="#">Início</a>
+        <a href="#">Serviços</a>
+        <a href="#">Sobre</a>
+        <a href="#" className="btn-contatos">
+          Contato
+        </a>
+      </nav>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
